1536f5bc2ac21d802d71eb9ec312826fe7b0262e
“M0ther0fGingers” <jen@robisons.us>
2025-04-05 15:11:34 -0500

    added the pointToLayerr and onEachFeature sections

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -9,9 +9,9 @@ let basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
 
 // Create the map object with center and zoom options.
 // Centered map on Los Angeles
-let map = L.map("map-id", {
+let map = L.map("map", {
   center: [34.052235, -118.243683],
-  zoom: 13,
+  zoom: 4,
   // Then add the 'basemap' tile layer to the map.
   layers: [basemap]
 });
@@ -24,13 +24,21 @@ let map = L.map("map-id", {
 
 // Make a request that retrieves the earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+let earthquakeData = data;
+console.log(earthquakeData)
+});
 
   // This function returns the style data for each of the earthquakes we plot on
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
-
-    
+    return {
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      radius: getRadius(feature.properties.mag),
+      fillOpacity: .75,
+      color: "white",
+      weight: 0.5,
+    };
   }
 
   // This function determines the color of the marker based on the depth of the earthquake.
@@ -64,20 +72,29 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       // Add the popup to the map
       // }.bindPopup(`</h1> <hr> <h3>Magnitude: ${properties[i].mag.toLocaleString()}</h3>`).addTo(myMap);
       // magnitudeMarkers.push(radius)
-    )
-    });
+      }
+     )
+    };
+  // );
 
   // Add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
     // Turn each feature into a circleMarker on the map.
     pointToLayer: function (feature, latlng) {
-
+      // Create a circle marker for each feature
+      return L.circleMarker(latlng, {
+        radius: feature.properties.mag * 4, 
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "white",
+        weight: 1,
+        fillOpacity: 0.75
+      })
     },
     // Set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
-
+      layer.bindPopup(`<h3>${feature.properties.mag}</h3><hr><p>${new Date(feature.geometry.coordinates)}</p>`);
     }
   // OPTIONAL: Step 2
   // Add the data to the earthquake layer instead of directly to the map.
@@ -93,7 +110,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     let div = L.DomUtil.create("div", "info legend");
 
     // Initialize depth intervals and colors for the legend
-
+    
 
     // Loop through our depth intervals to generate a label with a colored square for each interval.
 
@@ -113,4 +130,4 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     // Then add the tectonic_plates layer to the map.
 
   });
-});
+// });
