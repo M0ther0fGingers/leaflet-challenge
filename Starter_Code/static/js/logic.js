6e5a26ddc664788683f6168caf550d145caa731f
“M0ther0fGingers” <jen@robisons.us>
2025-04-06 10:39:49 -0500

    finished part 1

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -26,7 +26,7 @@ let map = L.map("map", {
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
 let earthquakeData = data;
 console.log(earthquakeData)
-});
+// });
 
   // This function returns the style data for each of the earthquakes we plot on
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
@@ -43,37 +43,57 @@ console.log(earthquakeData)
 
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
+    if (depth > 90) {
+      return "#fc0f03";
+    }
+    if (depth > 70) {
+      return "#f67103";
+    }
+    if (depth > 50) {
+      return "#f6a603";
+    }
+    if (depth > 30) {
+      return "#f6ec03";
+    }    
+    if (depth > 10) {
+      return "#59a511";
+    }
+    return "#7cf905";
+
     // Initialize an array to hold the depth
-    let depthMarkers = [];
-    // Use .forEach to loop through the array
-      depth.forEach((feature) => {
-      let depthCoordinate = feature.features.geometry.coordinates[2];
-            // Assign color based on depth. The depth coordinate is measure in kilometers. According to the USGS site
-            // 0-70 km is shallow, 70-300 km is intermediate, and 300-700 km is deep. 
-            let markerColor = depthCoordinate < 70 ? "#d4ff33" : // Light green for a shallow quake
-                              depthCoordinate < 300 ? "#bfe728" : // Medium green for an intermediate quake
-                              depthCoordinate < 700 ? "#829d1b" : "#000000 "; // Dark green for a deep quake, black for all else
-            depthMarkers.push(markerColor)
-                            });
+    // let depthMarkers = [];
+    // // Use .forEach to loop through the array
+    //   depth.forEach((feature) => {
+    //   let depthCoordinate = feature.features.geometry.coordinates[2];
+    //         // Assign color based on depth. The depth coordinate is measure in kilometers. According to the USGS site
+    //         // 0-70 km is shallow, 70-300 km is intermediate, and 300-700 km is deep. 
+    //         let markerColor = depthCoordinate < 70 ? "#d4ff33" : // Light green for a shallow quake
+    //                           depthCoordinate < 300 ? "#bfe728" : // Medium green for an intermediate quake
+    //                           depthCoordinate < 700 ? "#829d1b" : "#000000 "; // Dark green for a deep quake, black for all else
+    //         depthMarkers.push(markerColor)
+    //                         });
   };
 
   // This function determines the radius of the earthquake marker based on its magnitude.
+  // Question for tutor, I calculated the coordinates and radius, how do I get those back into the magnitudeMarkers variable?
   function getRadius(magnitude) {
+    // if the magnitude is 0, 
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
     // Initalize an array to hold the radius
-    let magnitudeMarkers = [];
-    // use forEach to find all radii
-    features.properties.mag.forEach((feature) => {
-      // Get the earthquate magnitude
-      let magnitude = features.properties.mag;
-      // Get the coordinates
-      let coordinates = features.geometry.coordinates;
-      // Assign radius based on magnitude. 
-      let radius = markerSize(magnitude);  
-      // Add the popup to the map
-      // }.bindPopup(`</h1> <hr> <h3>Magnitude: ${properties[i].mag.toLocaleString()}</h3>`).addTo(myMap);
-      // magnitudeMarkers.push(radius)
-      }
-     )
+    // let magnitudeMarkers = [];
+    // // use forEach to find all radii
+    // features.properties.mag.forEach((feature) => {
+    //   // Get the earthquate magnitude
+    //   let magnitude = feature.features.properties.mag;
+    //   // Get the coordinates
+    //   let coordinates = feature.features.geometry.coordinates;
+    //   // Assign radius based on magnitude. 
+    //   let radius = markerSize(magnitude);  
+    //   // Add the popup to the map
+      // }.bindPopup(`</h1> <hr> <h3>Magnitude: ${properties[i].mag.toLocaleString()}</h3>`).addTo(myMap), magnitudeMarkers.push(radius)
     };
   // );
 
@@ -94,7 +114,7 @@ console.log(earthquakeData)
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
-      layer.bindPopup(`<h3>${feature.properties.mag}</h3><hr><p>${new Date(feature.geometry.coordinates)}</p>`);
+      layer.bindPopup(`<h3>Magnitude: ${feature.properties.mag}</h3><hr><h3>Depth: ${feature.geometry.coordinates[2]}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
     }
   // OPTIONAL: Step 2
   // Add the data to the earthquake layer instead of directly to the map.
@@ -105,29 +125,40 @@ console.log(earthquakeData)
     position: "bottomright"
   });
 
-  // Then add all the details for the legend
-  legend.onAdd = function () {
-    let div = L.DomUtil.create("div", "info legend");
-
-    // Initialize depth intervals and colors for the legend
-    
-
-    // Loop through our depth intervals to generate a label with a colored square for each interval.
-
+// Then add all the details for the legend
+legend.onAdd = function (map) {
+// Initialize depth intervals and colors for the legend
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 30, 50, 70, 90],
+        labels = ["#7cf905", "#59a511", "#f6ec03", "#f6a603", "#f67103", "#fc0f03"];
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + labels[i] + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
 
     return div;
-  };
+};
 
-  // Finally, add the legend to the map.
+legend.addTo(map);
 
+  //   // Loop through our depth intervals to generate a label with a colored square for each interval.
+
+
+  //   return div;
+  // };
+
+  // // Finally, add the legend to the map.
+  // legend.addTo(map);
 
   // OPTIONAL: Step 2
   // Make a request to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
-    // Save the geoJSON data, along with style information, to the tectonic_plates layer.
+  // d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
+  //   // Save the geoJSON data, along with style information, to the tectonic_plates layer.
 
 
-    // Then add the tectonic_plates layer to the map.
+  //   // Then add the tectonic_plates layer to the map.
 
-  });
-// });
+  // });
+});
